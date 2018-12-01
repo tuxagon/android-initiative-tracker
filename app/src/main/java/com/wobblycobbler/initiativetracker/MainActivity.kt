44527fd6f2d74809604cbf6c41951d2fb36437cd
Kenneth Bogner <kenneth.bogner@assurant.com>
2018-11-30 21:13:29 -0500

    Migrate to androidx

--- app/src/main/java/com/wobblycobbler/initiativetracker/MainActivity.kt
+++ app/src/main/java/com/wobblycobbler/initiativetracker/MainActivity.kt
@@ -2,8 +2,8 @@ package com.wobblycobbler.initiativetracker
 
 import android.graphics.Color
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity;
 import android.widget.ListView
 import android.view.Menu
 import android.view.MenuItem
